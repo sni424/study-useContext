a6e2ae7e135faa5009a6e74013e028b0f76c5bb1
jong <sni424@naver.com>
2022-07-28 22:22:43 +0900

    useRef기초 렌더링 공부

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import { Route, Routes } from "react-router-dom";
 
 import Main from "./Pages/Main";
+import TodoList from "./Pages/TodoList";
 import UseContextStudy from "./Pages/UseContext";
 import DarkPage from "./Pages/UseContext/DarkPage";
+import UseRefStudy from "./Pages/UseRef";
 
 const App = () => {
     return (
         <div>
             <Routes>
                 <Route path="/" element={<Main />} />
-                <Route path="/context" element={<UseContextStudy />} />
-                <Route path="/dark" element={<DarkPage />} />
+                <Route path="context" element={<UseContextStudy />} />
+                <Route path="dark" element={<DarkPage />} />
+                <Route path="todo" element={<TodoList />} />
+                <Route path="useref" element={<UseRefStudy />} />
             </Routes>
         </div>
     );
