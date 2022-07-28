b23abd154161a175f1a834d7a6b809dfc78cfe38
jong <sni424@naver.com>
2022-07-28 22:47:59 +0900

    useRef dom input이나 textarea에 넣으면 좋음

--- src/Pages/UseRef/index.jsx
+++ src/Pages/UseRef/index.jsx
@@ -3,8 +3,10 @@ import React, { useState, useRef, useEffect } from "react";
 const UseRefStudy = () => {
     const [count, setCount] = useState(0);
     const [render, setRender] = useState(0);
+    const [inputValue, setInputValue] = useState("");
     const countRef = useRef(0);
     const countVar = 0;
+    const inputRef = useRef();
 
     const changeState = () => {
         setCount(count + 1);
@@ -19,7 +21,7 @@ const UseRefStudy = () => {
 
     useEffect(() => {
         setRender(render + 1);
-        console.log("렌더링", render);
+        inputRef.current.focus();
     }, []);
 
     return (
@@ -27,9 +29,27 @@ const UseRefStudy = () => {
             <h1>State : {count}</h1>
             <h1>Ref : {countRef.current}</h1>
             <h1>countVar : {countVar}</h1>
-            <button onClick={changeState}>use업버튼</button>
-            <button onClick={changeRef}>ref업버튼</button>
-            <button onClick={changeVar}>var업버튼</button>
+            <input
+                ref={inputRef}
+                type="text"
+                placeholder="입력해주세요."
+                value={inputValue}
+                onChange={(e) => {
+                    setInputValue(e.target.value);
+                }}
+            />
+            <button
+                onClick={() => {
+                    setInputValue("");
+                }}
+            >
+                로그인
+            </button>
+            <div>
+                <button onClick={changeState}>use업버튼</button>
+                <button onClick={changeRef}>ref업버튼</button>
+                <button onClick={changeVar}>var업버튼</button>
+            </div>
         </div>
     );
 };
