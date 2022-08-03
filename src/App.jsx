f5c0ddffbe554c8826c4f0c009652234301af0d4
jong <sni424@naver.com>
2022-08-03 23:17:33 +0900

    theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { Route, Routes } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
 
 import Main from "./Pages/Main";
 import StyledComponent from "./Pages/StyledComponent";
+import Theme from "./Pages/StyledComponent/Theme";
 import TodoList from "./Pages/TodoList";
 import UseCallBackStudy from "./Pages/UseCallback";
 import UseContextStudy from "./Pages/UseContext";
@@ -12,22 +14,35 @@ import UseReducerStudy from "./Pages/UseReducer";
 import DeepReducer from "./Pages/UseReducer/Deep";
 import UseRefStudy from "./Pages/UseRef";
 
+const darkTheme = {
+    textColor: "whitesmoke",
+    backgroundColor: "#111",
+};
+
+const lightTheme = {
+    textColor: "#111",
+    backgroundColor: "whitesmoke",
+};
+
 const App = () => {
     return (
         <div>
-            <Routes>
-                <Route path="/" element={<Main />} />
-                <Route path="context" element={<UseContextStudy />} />
-                <Route path="dark" element={<DarkPage />} />
-                <Route path="todo" element={<TodoList />} />
-                <Route path="useref" element={<UseRefStudy />} />
-                <Route path="usememo" element={<UseMemoStudy />} />
-                <Route path="usememo1" element={<OtherMemo />} />
-                <Route path="usecallback" element={<UseCallBackStudy />} />
-                <Route path="usereducer" element={<UseReducerStudy />} />
-                <Route path="usereducer1" element={<DeepReducer />} />
-                <Route path="style" element={<StyledComponent />} />
-            </Routes>
+            <ThemeProvider theme={lightTheme}>
+                <Routes>
+                    <Route path="/" element={<Main />} />
+                    <Route path="context" element={<UseContextStudy />} />
+                    <Route path="dark" element={<DarkPage />} />
+                    <Route path="todo" element={<TodoList />} />
+                    <Route path="useref" element={<UseRefStudy />} />
+                    <Route path="usememo" element={<UseMemoStudy />} />
+                    <Route path="usememo1" element={<OtherMemo />} />
+                    <Route path="usecallback" element={<UseCallBackStudy />} />
+                    <Route path="usereducer" element={<UseReducerStudy />} />
+                    <Route path="usereducer1" element={<DeepReducer />} />
+                    <Route path="style" element={<StyledComponent />} />
+                    <Route path="theme" element={<Theme />} />
+                </Routes>
+            </ThemeProvider>
         </div>
     );
 };
--- src/Pages/StyledComponent/index.jsx
+++ src/Pages/StyledComponent/index.jsx
@@ -1,17 +1,58 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 const Father = styled.div`
     display: flex;
+`;
+
+const rotateAnime = keyframes`
+0%{
+transform: rotate(0deg);
+background-color: black;
+color: white;
+}50%{
+transform: rotate(180deg);
+background-color: green;
+color: blue;
+}
+100%{
+    transform: rotate(0deg);
+    background-color: tomato;
+    color: white;
+}
+`;
+
+const hoverAnime = keyframes`
+from{
+    padding: 0.25em 1em;
+}to{
+    padding: 0.5em 1.5em;
+}
 `;
 const BoxOne = styled.div`
     background-color: teal;
     width: 100px;
     height: 100px;
 `;
+const TextP = styled.p`
+    font-size: 0.5rem;
+`;
 const BoxTwo = styled.div`
     background-color: tomato;
     width: 100px;
     height: 100px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    animation: ${rotateAnime} 1s linear;
+    span {
+        font-size: 12px;
+        font-weight: 800;
+    }
+    ${TextP} {
+        &:hover {
+            font-size: 18px;
+        }
+    }
 `;
 const Text = styled.span`
     color: white;
@@ -20,6 +61,10 @@ const Button = styled.button`
     padding: 0.25em 1em;
     border: 2px solid palevioletred;
     cursor: pointer;
+    &:hover {
+        animation: ${hoverAnime} 0.5s;
+        animation-fill-mode: forwards;
+    }
 `;
 
 const TomatoButton = styled(Button)`
@@ -35,7 +80,10 @@ const StyledComponent = () => {
             <BoxOne>
                 <Text>Hello</Text>
             </BoxOne>
-            <BoxTwo />
+            <BoxTwo>
+                <span>적당히 바람이 시원해!</span>
+                <TextP>알았어! 알았어!</TextP>
+            </BoxTwo>
             <Button>안녕</Button>
             <TomatoButton>반가워</TomatoButton>
             <Button as="a" href="https://naver.com" target="_blank">
